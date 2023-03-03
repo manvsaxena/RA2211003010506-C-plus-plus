3440ab4734adb36248b22c02d701e74bd503d778
manvsaxena <126712194+manvsaxena@users.noreply.github.com>
2023-03-03 13:31:09 +0530

    while_problem file
    Problem: 
    
    You are given a program that outputs all the numbers from 0 to 20.
    Change the code to make it output only numbers that are multiples of 3.

---
+++ while_program.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    //change the code
+    int num = 1;
+    while(num<=20){
+        multiple=num%3==0;
+        cout<<multiple<<endl;
+        num+=1;
+    }
+    
+    return 0;
+}
