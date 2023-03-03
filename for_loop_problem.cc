c50be52663d36b2da309459088acb65db0a69e9b
manvsaxena <126712194+manvsaxena@users.noreply.github.com>
2023-03-03 14:00:52 +0530

    for_loop_problem file
    Problem:
    
    You are on a 5 hour sea voyage. The ship sails at a speed of 40 km per hour. Write a program that will output how many kilometers the ship has traveled by each hour.

---
+++ for_loop_problem.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int distance = 0;
+    //your code goes here
+    
+    for(int i=1;i<=5;i++){
+        distance=40*i;
+        cout<<distance<<endl;
+    }
+    return 0;
+}
