f82cbc9182bed8f399fb4430be1bd9fd8078f904
manvsaxena <126712194+manvsaxena@users.noreply.github.com>
2023-03-03 13:25:08 +0530

    if_else_program_1 file
    Problem:
    
    The else statement
    
    
    To study at a university, you must score higher than or equal to 90 out of 100 points on an exam.
    The given program takes points as input.
    
    Task:
    Complete the code to output "pass" if the score is higher than or equal to 90, otherwise print "fail".
    
    Sample Input:
    95
    
    Sample Output:
    pass

---
+++ if_else_program.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+        double points;
+        cin >> points;
+
+        //your code goes here
+
+        if(point>=90){
+                cout<<"pass"<<endl;
+        }
+        else{
+                cout<<"fail"<<endl;
+        }
+	return 0;
+}
