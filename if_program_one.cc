e83da2a15dbdf68b340a24ce3edd6fd510f68062
manvsaxena <126712194+manvsaxena@users.noreply.github.com>
2023-03-03 13:19:54 +0530

    if program-1 file
    Problem: 
    
    Admission to the pool is free for children under 7 years of age.
    The given program takes age as an input.
    
    Task:
    Complete the code to output "free" if the child's age is less than 7.
    
    Sample Input:
    6
    
    Sample Output:
    free

---
+++ if_program_one.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+        int age;
+        cin >> age;
+
+        // your code goes here
+        if(age<7){
+                cout<<"free"<<endl;
+        }
+	return 0;
+}
